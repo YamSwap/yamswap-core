41339fc94f82514ee2dbdea684e62c1117beafac
a186r <hi.aewalle@gmail.com>
2020-11-16 15:17:00 +0800

    使用prettier插件，使用mocha测试

--- test/YamswapERC20.spec.ts
+++ test/YamswapERC20.spec.ts
@@ -1,8 +1,11 @@
+import chai, {expect} from 'chai'
 import {Contract} from 'ethers'
 import {solidity, MockProvider, deployContract} from "ethereum-waffle";
 import {expandTo18Decimals} from "./shared/utilities";
 
 import ERC20 from '../build/ERC20.json'
+import {keccak256, defaultAbiCoder, toUtf8Bytes} from "ethers/utils";
+
 chai.use(solidity)
 
 const TOTAL_SUPPLY = expandTo18Decimals(10000)
@@ -19,5 +22,39 @@ describe('YamswapERC20', () => {
     let token: Contract
     beforeEach(async () => {
         token = await deployContract(wallet, ERC20, [TOTAL_SUPPLY])
+    })
+
+    it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SPEARATOR, PERMIT_TYPEHASH', async () => {
+        const name = await token.name()
+        expect(name).to.eq('Yamswap')
+        expect(await token.symbol().to.eq('YAM'))
+        expect(await token.decimals().to.eq(18))
+        expect(await token.totalSupply().to.eq(TOTAL_SUPPLY))
+        expect(await token.DOMAIN_SEPARATOR()).to.eq(
+            keccak256(
+                defaultAbiCoder.encode(
+                    ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
+                    [
+                        keccak256(
+                            toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
+                        ),
+                        keccak256(toUtf8Bytes(name)),
+                        keccak256(toUtf8Bytes('1')),
+                        1,
+                        token.address
+                    ]
+                )
+            )
+        )
+        expect(await token.PERMIT_TYPEHASH()).to.eq(
+            keccak256(toUtf8Bytes('Permit(address owner, address spender, uint256 value, uint256 nonce, uint256 deadline)'))
+        )
+    })
+
+    it('approve', async () => {
+        await expect(token.approve(other.address, TEST_AMOUNT))
+            .to.emit(token, 'Approval')
+            .withArgs(wallet.address, other.address, TEST_AMOUNT)
+        expect(await token.allowance(wallet.address, other.address)).to.eq(TEST_AMOUNT)
     })
 })
