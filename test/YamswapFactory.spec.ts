906c56958fa0402d8950a3ba0ec3462abdabd52c
a186r <hi.aewalle@gmail.com>
2020-11-17 17:41:59 +0800

    setFeeToSetter测试

--- test/YamswapFactory.spec.ts
+++ test/YamswapFactory.spec.ts
@@ -69,4 +69,11 @@ describe('YamswapFactory', () => {
     await factory.setFeeTo(wallet.address)
     expect(await factory.feeTo()).to.eq(wallet.address)
   })
+
+  it('setFeeToSetter', async () => {
+    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('Yamswap: FORBIDDEN')
+    await factory.setFeeToSetter(other.address)
+    expect(await factory.feeToSetter()).to.eq(other.address)
+    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('Yamswap: FORBIDDEN')
+  })
 })
