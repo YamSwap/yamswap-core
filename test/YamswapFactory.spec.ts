957e38fde18c496c6b324d6577082ff8d5c0700e
a186r <hi.aewalle@gmail.com>
2020-11-17 16:06:20 +0800

    createPair测试完成

--- test/YamswapFactory.spec.ts
+++ test/YamswapFactory.spec.ts
@@ -3,6 +3,9 @@ import { solidity, MockProvider, createFixtureLoader} from 'ethereum-waffle'
 import { Contract } from 'ethers'
 import { AddressZero } from 'ethers/constants'
 import { factoryFixture } from './shared/fixtures'
+import YamswapPair from '../build/YamswapPair.json'
+import { getCreate2Address } from './shared/utilities'
+import { bigNumberify } from 'ethers/utils'
 
 chai.use(solidity)
 
@@ -30,5 +33,24 @@ describe('YamswapFactory', () => {
     expect(await factory.feeTo()).to.eq(AddressZero)
     expect(await factory.feeToSetter()).to.eq(wallet.address)
     expect(await factory.allPairsLength()).to.eq(0)
+  })
+
+  async function createPair(tokens: [string, string]) {
+    const bytecode = `0x${YamswapPair.evm.bytecode.object}`
+    const create2Address = getCreate2Address(factory.address, tokens, bytecode)
+    await expect(factory.createPair(...tokens))
+      .to.emit(factory, 'PairCreated')
+      .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, bigNumberify(1))
+
+    await expect(factory.createPair(...tokens)).to.be.reverted // PAIR_EXISTS
+    await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted // PAIR_EXISTS
+    expect(await factory.getPair(...tokens)).to.eq(create2Address)
+    expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address)
+    expect(await factory.allPairs(0)).to.eq(create2Address)
+    expect(await factory.allPairsLength()).to.eq(1)
+  }
+
+  it('createPair', async () => {
+    await createPair(TEST_ADDRESSES)
   })
 })
