d7b414c714ccade68f5e875ca5407323ae7b568c
a186r <hi.aewalle@gmail.com>
2020-11-18 15:43:05 +0800

    pair:mint

--- test/YamswapPair.spec.ts
+++ test/YamswapPair.spec.ts
@@ -1,12 +1,16 @@
 import chai, {expect} from 'chai'
+import {Contract} from 'ethers'
 import { BigNumber, bigNumberify } from 'ethers/utils'
-import { solidity, MockProvider } from 'ethereum-waffle'
+import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
+import {pairFixture} from './shared/fixtures'
+import { expandTo18Decimals } from './shared/utilities'
+import {AddressZero} from 'ethers/constants'
 
 const MINIMUM_LIQUIDITY = bigNumberify(10).pow(3)
 
 chai.use(solidity)
 
-const override = {
+const overrides = {
   gasLimit: 9999999
 }
 
@@ -18,4 +22,37 @@ describe('YamswapPair', () => {
   })
 
   const [wallet, other] = provider.getWallets()
+  const loadFixture = createFixtureLoader(provider, [wallet])
+
+  let factory: Contract
+  let token0: Contract
+  let token1: Contract
+  let pair: Contract
+
+  beforeEach(async () => {
+    const fixture = await loadFixture(pairFixture)
+    factory = fixture.factory
+    token0 = fixture.token0
+    token1 = fixture.token1
+    pair = fixture.pair
+  })
+
+  it('mint', async () => {
+    const token0Amount = expandTo18Decimals(1)
+    const token1Amount = expandTo18Decimals(4)
+    await token0.transfer(pair.address, token0Amount)
+    await token1.transfer(pair.address, token1Amount)
+
+    const expectedLiquidity = expandTo18Decimals(2)
+    await expect(pair.mint(wallet.address, overrides))
+      .to.emit(pair, 'Transfer')
+      .withArgs(AddressZero, AddressZero, MINIMUM_LIQUIDITY)
+      .to.emit(pair, 'Transfer')
+      .withArgs(AddressZero, wallet.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
+      .to.emit(pair, 'Sync')
+      .withArgs(token0Amount, token1Amount)
+      .to.emit(pair, 'Mint')
+      .withArgs(wallet.address, token0Amount, token1Amount)
+
+  })
 })
